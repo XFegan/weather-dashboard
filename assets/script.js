ff7f3799681c106a611e8b9caec503a6090d51f4
XFegan <xavierslf11@gmail.com>
2021-07-02 20:05:32 -0400

    vars created from index

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,16 @@
+var searchButtonEl = document.getElementById("search-button");
+var cityEl = document.getElementById("enter-city");
+var clearEl = document.getElementById("clear-history");
+var nameEl = document.getElementById("city-name");
+var currentPictureEl = document.getElementById("current-picture")
+var currentTempEl = document.getElementById("temperature");
+var currentWindEl = document.getElementById("wind-speed");
+var currentHumidityEl = document.getElementById("humidity");
+var currentUVEl = document.getElementById("UV-index");
+var hsitoryEl = document.getElementById("history");
+var fiveDayEl = document.getElementById("five-day");
+var todayWeatherEl = document.getElementById("today-weather");
+var searchHistory = JSON.parse(localStorage.getItem("search")) || [];
 currentCity = "Storrs";
 var apiBase = "https://api.openweathermap.org/data/2.5/weather?q=";
 var apiKey = "&units=imperial&appid=9a14f6c7f1d7fe60a328bfd813252503";
@@ -8,4 +21,6 @@ fetch(apiBase + currentCity + apiKey)
     })
     .then(function (data){
         console.log(data);
+
+
     })
